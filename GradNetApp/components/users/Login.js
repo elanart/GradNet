bddb32a6521077095fec581fd4dbbee169c398d2
elanart <elanart.06@gmail.com>
2024-05-26 10:07:51 +0700

    [Lan] - dang nhap

--- GradNetApp/components/users/Login.js
+++ GradNetApp/components/users/Login.js
@@ -1,107 +1,78 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Text, View } from "react-native";
 import MyStyles from "../../styles/MyStyles";
-import React, { useContext, useState } from "react";
+import { useState } from "react";
+import { Button, TextInput } from "react-native-paper";
 import APIs, { authAPI, endpoints } from "../../configs/APIs";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { MyDispatcherContext } from "../../configs/Context";
 
 const Login = () => {
-  const [user, setUser] = useState({});
   const fields = [
     {
       label: "Tên đăng nhập",
       icon: "account",
-      name: "username",
+      field: "username",
     },
     {
       label: "Mật khẩu",
       icon: "eye",
+      field: "password",
       secureTextEntry: true,
-      name: "password",
     },
   ];
+
+  const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
-  const dispatch = useContext(MyDispatcherContext);
 
-  const login = async () => {
-    setLoading(true);
-
-    try {
-      let res = await APIs.post(endpoints["login"], {
-        ...user,
-        client_id: "EIFOWs5h8FPJPluYFvXIQdcLvAWfjn8AHXwrW3AG ",
-        client_secret:
-          "o3YdzEnDROX2W25N15Ar8PDdOxwYHALSfrxPEcuCHRDBXPHPPkAm3bDUrbawn5VE4FGEQaLyP8zrMGJdasYL9ZFrl77jQzK64sxB3kwevXMB5IWJLmsnAHhigeJJkJQL",
-        grant_type: "password",
-      });
-
-      AsyncStorage.setItem("access-token", res.data.access_token);
-
-      setTimeout(async () => {
-        let token = await AsyncStorage.getItem("access-token");
-        let user = await authAPI(token).get(endpoints["current-user"]);
-        AsyncStorage.setItem("user", JSON.stringify(user.data));
-
-        dispatch({
-          type: "login",
-          payload: user.data,
-        });
-
-        // nav.navigate("Home");
-      }, 100);
-    } catch (ex) {
-      console.error(ex);
-      Alert.alert(
-        "Login failed",
-        "Please check your credentials and try again."
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const updateState = (field, value) => {
+  const change = (value, field) => {
     setUser((current) => {
       return { ...current, [field]: value };
     });
   };
 
+  const login = async () => {
+    setLoading(true);
+    try {
+      let res = await APIs.post(endpoints["login"], {
+        ...user,
+        client_id: "EIFOWs5h8FPJPluYFvXIQdcLvAWfjn8AHXwrW3AG",
+        client_secret:
+          "o3YdzEnDROX2W25N15Ar8PDdOxwYHALSfrxPEcuCHRDBXPHPPkAm3bDUrbawn5VE4FGEQaLyP8zrMGJdasYL9ZFrl77jQzK64sxB3kwevXMB5IWJLmsnAHhigeJJkJQL",
+        grant_type: "password",
+      });
+      console.info(res.data);
+      await AsyncStorage.setItem("token", res.data.access_token);
+
+      setTimeout(async () => {
+        let user = await authAPI(res.data.access_token).get(
+          endpoints["current-user"]
+        );
+        console.info(user.data);
+      }, 100);
+    } catch (ex) {
+      console.error(ex);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={[MyStyles.container, MyStyles.margin]}>
-      <ScrollView>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-          {fields.map((f) => (
-            <TextInput
-              value={user[f.name]}
-              onChangeText={(t) => updateState(f.name, t)}
-              key={f.label}
-              style={MyStyles.margin}
-              label={f.label}
-              secureTextEntry={f.secureTextEntry}
-              right={<TextInput.Icon icon={f.icon} />}
-            />
-          ))}
+      <Text style={MyStyles.subject}>Đăng nhập người dùng</Text>
+      {fields.map((f) => (
+        <TextInput
+          value={user[f.field]}
+          onChangeText={(t) => change(t, f.field)}
+          key={f.field}
+          label={f.label}
+          secureTextEntry={f.secureTextEntry}
+          style={MyStyles.margin}
+          right={<TextInput.Icon icon={f.icon} />}
+        />
+      ))}
 
-          <Button
-            style={MyStyles.margin}
-            loading={loading}
-            icon="account"
-            mode="contained"
-            onPress={login}
-          >
-            Đăng nhập
-          </Button>
-        </KeyboardAvoidingView>
-      </ScrollView>
+      <Button loading={loading} onPress={login} icon="account" mode="contained">
+        Đăng nhập
+      </Button>
     </View>
   );
 };
